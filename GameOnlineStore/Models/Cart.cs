8953cb2423907ef3f1436e08d111bca095700058
Алексей Чванов <a.chvanov@upz.ru>
2024-01-30 16:07:52 +0500

    Added models Cart and CartItem

---
+++ GameOnlineStore/Models/Cart.cs
@@ -0,0 +1,25 @@
+﻿namespace GameOnlineStore.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public List<CartItem> Items { get; set; }
+
+        public decimal Cost
+        {
+            get
+            {
+                return Items.Sum(item => item.Cost);
+            }
+        }
+
+        public int Amount
+        {
+            get
+            {
+                return Items.Sum(item => item?.Amount ?? 0);
+            }
+        }
+    }
+}
