c08504d9f76b693b7379252435b8d42ad28cc848
Alexey Chvanov <alexeusrus95@gmail.com>
2024-02-13 21:20:32 +0500

    Deleted Compare class, changed Model in the Index.chtml

--- GameOnlineStore/Models/Compare.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GameOnlineStore.Models
-{
-    public class Compare
-    {
-        public string UserId { get; set; }
-        public List<Product> CompareProducts { get; set; }
-    }
-}
