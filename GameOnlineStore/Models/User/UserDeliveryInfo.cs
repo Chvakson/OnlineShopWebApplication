8a8135f5e60520f808efbda7cf13eec936128121
Чванов Алексей Дмитриевич <a.chvanov@upz.ru>
2024-03-25 16:12:18 +0500

    Added mask for set phonec number into input.

--- GameOnlineStore/Models/User/UserDeliveryInfo.cs
+++ GameOnlineStore/Models/User/UserDeliveryInfo.cs
@@ -11,6 +11,7 @@ namespace GameOnlineStore.Models
         [Required(ErrorMessage = "Не указан email")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Не указан номер телефона")]
+        [RegularExpression(@"^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$", ErrorMessage = "Указан неверный номер телефона.")] 
         public string Phone { get; set; }
         public UserAddress UserAddress { get; set; }
         public string? Comment { get; set; }
