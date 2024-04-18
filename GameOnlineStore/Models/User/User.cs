c132d71465fdf2dfef10760a7191c5f441748861
Чванов Алексей Дмитриевич <a.chvanov@upz.ru>
2024-04-18 15:33:18 +0500

    added new class User

---
+++ GameOnlineStore/Models/User/User.cs
@@ -0,0 +1,24 @@
+﻿namespace GameOnlineStore.Models.User
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public Role Role { get; set; }
+
+        public User(Guid id, string login, string password, string firstName, string lastName, string phone)
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            Role = new Role { Name = "User" };
+        }
+    }
+}
